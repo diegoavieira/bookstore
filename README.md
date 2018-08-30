08b563e3a4b0e7d23ac6a5baae01f39133efe6a2
Diego Adilson Vieira <diegoavieira@gmail.com>
2018-08-30 16:16:13 -0300

    Changed project name to Recipe Book

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bookstore
+# Recipe Book
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.4.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to bookstore!');
+    expect(page.getParagraphText()).toEqual('Welcome to recipebook!');
   });
 });
